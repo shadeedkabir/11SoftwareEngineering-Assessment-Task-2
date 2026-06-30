a124fd5118cd174b313c53cf803e707e51bbc4c1
shadeedkabir <shadeed.kabir@education.nsw.gov.au>
2026-07-01 02:21:14 +1000

    commit

--- 11SENG Assessment Task 2.py
+++ 11SENG Assessment Task 2.py
@@ -1,16 +1,11 @@
-from tkinter import *#imports all symbols from the tkinter module into the current name:
-def button_click():
-    label2=Label(root, text="Welcome to the multiplication quiz!")
-    label2.place(x=150, y=50)
+# Imports
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import random
+import os
 
-
-root =Tk()
-root.title("Example of Button")
-root.geometry("400x400") #make the window 400 x 400 size
-
-
-button1=Button(root,text="Start", command=button_click)
-button1.place(x=165,y=300)
-
-
-root.mainloop()
+# Windows
+root = Tk()
+root.title("Worlds Flag Quiz")
+root.geometry("900x600")
